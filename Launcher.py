d6b507cb0da58cb5ec26c1103fb1b69cea53ced4
julianftoro <juliant@ite-consult.com>
2021-02-12 11:27:56 -0300

    Demand generation + minor changes

--- Launcher.py
+++ Launcher.py
@@ -10,6 +10,7 @@ from tkinter import ttk
 import pandas as pd
 import threading
 import sqlalchemy
+import datetime
 #import sqlalchemy_hana
 
 connection_to_HANA = None
@@ -27,13 +28,14 @@ def generate_breakout_file(BOM, ItemMaster, Facility):  #TODO esto funciona asum
     #TODO filtrar los casos que vienen con sumas de Quantity igual a 0
     #TODO mirar solo la bomcode de la default facility
     #Merging with ItemMaster to keep only 'FG' category items
-    BREAKOUT = BOM[['ItemNumber', 'Facility', 'BomCode', 'ComponentItemNumber', 'Quantity']].merge(ItemMaster[['ItemNumber', 'CategoryCode']], on = 'ItemNumber', how = 'left')
+    BREAKOUT = BOM[['ItemNumber', 'Facility', 'BomCode', 'ComponentItemNumber', 'Quantity']].merge(ItemMaster[['ItemNumber', 'CategoryCode']].groupby('ItemNumber').first(), on = 'ItemNumber', how = 'left')
     BREAKOUT = BREAKOUT.query('CategoryCode == "FG"').drop('CategoryCode', axis = 1)
     #Second merging with ItemMaster to keep only 'INT' category component items
-    BREAKOUT = BREAKOUT.merge(ItemMaster[['ItemNumber', 'CategoryCode']], left_on = 'ComponentItemNumber', right_on = 'ItemNumber', suffixes = ['','_y']).drop('ItemNumber_y', axis = 1)
+    BREAKOUT = BREAKOUT.merge(ItemMaster[['ItemNumber', 'CategoryCode']].groupby('ItemNumber').first(), left_on = 'ComponentItemNumber', right_on = 'ItemNumber', suffixes = ['','_y']).drop('ItemNumber_y', axis = 1)
     BREAKOUT = BREAKOUT.query('CategoryCode == "INT"').drop('CategoryCode', axis = 1)
     #Blend percentage calculation
-    BREAKOUT = BREAKOUT[['ItemNumber', 'Facility', 'BomCode', 'Quantity']].groupby(['ItemNumber', 'Facility', 'BomCode']).sum().rename({'Quantity': 'BlendPercentage'}, axis = 1).query('BlendPercentage != 0').merge(BREAKOUT, left_index = True, right_on = ['ItemNumber', 'Facility', 'BomCode'], how = 'left')
+    BREAKOUT['Quantity'] = BREAKOUT['Quantity'].astype(float)
+    BREAKOUT = BREAKOUT[['ItemNumber', 'Facility', 'BomCode', 'Quantity']].groupby(['ItemNumber', 'Facility', 'BomCode']).sum().rename({'Quantity': 'BlendPercentage'}, axis = 1).query('BlendPercentage != 0').merge(BREAKOUT, left_index = True, right_on = ['ItemNumber', 'Facility', 'BomCode'], how = 'right')
     BREAKOUT['BlendPercentage'] = BREAKOUT['Quantity']/BREAKOUT['BlendPercentage']
     BREAKOUT.drop('Quantity', axis = 1, inplace = True)
     #Set column order
@@ -63,6 +65,39 @@ def generate_packlines_and_extruders(RoutingAndRates, WorkCenters, ItemMaster):
     
     return PACKLINES, EXTRUDERS
 
+def generate_demand(WorkOrders, ItemMaster, Model_Workcenters):
+    DEMAND = WorkOrders.merge(ItemMaster[['ItemNumber', 'Description', 'CustomerName', 'CategoryCode']], on = 'ItemNumber', how = 'left')
+    fg_filter = DEMAND['CategoryCode'] == 'FG'
+    DEMAND = DEMAND[fg_filter]
+    DEMAND['Due date'] = pd.to_datetime(DEMAND['PlannedEnd'])
+    DEMAND['Raw material date'] = DEMAND['Due date'] - datetime.timedelta(15)
+    DEMAND['Inventory'] = 0
+    DEMAND['Priority'] = 0
+    DEMAND['Priority product'] = 0
+    DEMAND = DEMAND.merge(Model_Workcenters[['WorkCenter', 'Model plant']], on = 'WorkCenter', how = 'left')
+    DEMAND['Purchase order'] = 'missing'
+    DEMAND['Entity'] = 'CJFoods'
+    DEMAND['Sales order'] = 'missing'
+    DEMAND['Process date'] = 'week ' + str(datetime.date.today().isocalendar()[1] + 3)
+    DEMAND.rename({'ItemNumber': 'Finished good', 'CustomerName': 'Customer', 'PlannedQty': 'Demand quantity (pounds)',
+                   'PlannedEnd': 'Due date', 'Model plant': 'Location', 'WorkCenter': 'Packline', 'WorkOrderNumber': 'Work order'}, axis = 1, inplace = True)
+    DEMAND.drop(['Facility', 'OrderStatus', 'CompletedQty', 'PlannedStart', 'Operation', 'CategoryCode'], axis = 1, inplace = True)
+    DEMAND = DEMAND[['Finished good', 'Description', 'Customer', 'Inventory', 'Priority product', 'Priority', 'Raw material date', 
+                     'Demand quantity (pounds)', 'Due date', 'Location', 'Purchase order', 'Sales order', 'Entity', 'Work order', 
+                     'Packline', 'Process date']]
+
+    return DEMAND
+
+def generate_inventory_bulk(Inventory, ItemMaster):
+    #TODO hay que repensarla cuando arreglen lo de la Facility en el ItemMaster
+    INVENTORY_BULK = Inventory.query('ItemStatus == "A"').merge(ItemMaster[['ItemNumber', 'CategoryCode']].groupby('ItemNumber').first(), on = 'ItemNumber', how = 'left')
+    INVENTORY_BULK.query('CategoryCode == "FG"', inplace = True)
+    INVENTORY_BULK.drop('ItemStatus', axis = 1, inplace = True)
+    INVENTORY_BULK['StockQuantity'] = INVENTORY_BULK['StockQuantity'].astype(float)
+    INVENTORY_BULK = INVENTORY_BULK.groupby(['ItemNumber', 'Facility']).sum()
+    
+    return INVENTORY_BULK
+
 def update_db_from_SAGE():
     
     #Connect to HANA
@@ -79,7 +114,7 @@ def update_db_from_SAGE():
     #Reads tables from REST services
     for table in table_urls:
         try:
-            globals()[table] = pd.read_json(table_urls[table], dtype = object)
+            globals()[table] = pd.read_json(table_urls[table], dtype = str)
             print(f'Table {table} succesfully loaded.')
         except Exception as e:
             print(f'Couldn\'t load table {table}: ' + str(e))
@@ -95,10 +130,10 @@ def update_db_from_SAGE():
             print(f'Couldn\'t save {table} table into HANA. ' + str(e))
             
     #Read manual files from HANA
-    manual_files = ['model_workcenters']
+    manual_files = ['Model_Workcenters']
     
     for table in manual_files:
-        globals()[table] = pd.read_sql_table(table, schema = 'manual_files', con = connection_to_HANA)
+        globals()[table] = pd.read_sql_table(table.lower(), schema = 'manual_files', con = connection_to_HANA)
             
     #Model files generation and uploading
     #1) Breakout
@@ -119,6 +154,13 @@ def update_db_from_SAGE():
         print('Extruders table succesfully generated and uploaded to HANA.')
     except Exception as e:
         print(f'Failed to generate and upload {current_table} to HANA: ' + str(e))
+    #3) Demand
+    try:
+        DEMAND = generate_demand(WorkOrders, ItemMaster, Model_Workcenters)
+        DEMAND.to_sql('demand', con = connection_to_HANA, if_exists = 'replace', index = False, schema = 'anylogic')
+        print('Demand table succesfully generated and uploaded to HANA.')
+    except Exception as e:
+        print('Failed to generate and upload Demand table to HANA: ' + str(e))
     #connection.close()
 
 def generate_model_files():
@@ -128,10 +170,20 @@ def generate_model_files():
     
     tables = ['BOM', 'Inventory', 'Facility', 'ItemMaster', 'RoutingAndRates', 'WorkCenters', 'WorkOrders']
     
-    #Read tables from HANA
+    #Read SAGE tables from HANA
     for table in tables:
         try:
-            globals()[table] = pd.read_sql(sql = table.lower(), schema = 'anylogic', con = connection_to_HANA)
+            globals()[table] = pd.read_sql_table(table.lower(), schema = 'sage', con = connection_to_HANA)
+            print(f'Table {table} succesfully read from HANA.')
+        except Exception as e:
+            print('Couldn\'t read table {table} from HANA. ' + str(e))
+            
+    #Read manual files from HANA
+    manual_files = ['Model_Workcenters']
+    
+    for table in manual_files:
+        try:
+            globals()[table] = pd.read_sql_table(table.lower(), schema = 'manual_files', con = connection_to_HANA)
             print(f'Table {table} succesfully read from HANA.')
         except Exception as e:
             print('Couldn\'t read table {table} from HANA. ' + str(e))
@@ -140,22 +192,56 @@ def generate_model_files():
     #1) Breakout
     try:
         BREAKOUT = generate_breakout_file(BOM, ItemMaster, Facility)
-        BREAKOUT.to_sql('breakout_file', con = connection_to_HANA, if_exists = 'replace', index = False)
-        print('Breakout table succesfully generated and uploaded to HANA.')
+        print('Breakout table succesfully generated.')
     except Exception as e:
-        print('Failed to generate and upload Breakout table to HANA: ' + str(e))
+        print('Failed to generate Breakout table: ' + str(e))
+    else:
+        try:
+            BREAKOUT.to_sql('breakout_file', con = connection_to_HANA, if_exists = 'replace', index = False)
+            print('Breakout table succesfully uploaded to HANA.')
+        except Exception as e:
+            print('Failed to upload Breakout table to HANA: ' + str(e))
     #2) Packlines and extruders
     try:
-        current_table = 'Packlines'
         PACKLINES, EXTRUDERS = generate_packlines_and_extruders(RoutingAndRates, WorkCenters, ItemMaster)
-        PACKLINES.to_sql('packlines', con = connection, if_exists = 'replace', index = False)
-        print('Packlines table succesfully generated and uploaded to HANA.')
-        current_table = 'Extruders'
-        EXTRUDERS.to_sql('extruders', con = connection, if_exists ='replace', index = False)
-        print('Extruders table succesfully generated and uploaded to HANA.')
+        print('Packlines and Extruders tables succesfully generated.')
     except Exception as e:
-        print(f'Failed to generate and upload {current_table} table to HANA: ' + str(e))
-    #connection.close()
+        print('Failed to generate Packlines and Extruders tables: ' + str(e))
+    else:
+        try:
+            PACKLINES.to_sql('packlines', con = connection_to_HANA, if_exists = 'replace', index = False)
+            print('Packlines table succesfully uploaded to HANA.')
+        except Exception as e:
+            print('Failed to upload Packlines table to HANA: ' + str(e))
+        try:
+            EXTRUDERS.to_sql('extruders', con = connection_to_HANA, if_exists ='replace', index = False)
+            print('Extruders table succesfully uploaded to HANA.')
+        except Exception as e:
+            print('Failed to upload Extruders table to HANA: ' + str(e))
+    #3) Demand
+    try:
+        DEMAND = generate_demand(WorkOrders, ItemMaster, Model_Workcenters)
+        print('Demand table succesfully generated.')
+    except Exception as e:
+        print('Failed to generate Demand table: ' + str(e))
+    else:
+        try:
+            DEMAND.to_sql('demand', con = connection_to_HANA, if_exists = 'replace', index = False)
+            print('Demand table succesfully uploaded to HANA.')
+        except Exception as e:
+            print('Failed to upload Demand table to HANA: ' + str(e))
+    #4) Inventory bulk
+    try:
+        INVENTORY_BULK = generate_inventory_bulk(Inventory, ItemMaster)
+        print('Inventory bulk table succesfully generated.')
+    except Exception as e:
+        print('Failed to generate Inventory bulk table: ' + str(e))
+    else:
+        try:
+            INVENTORY_BULK.to_sql('inventory_bulk', schema = 'anylogic', con = connection_to_HANA, if_exists = 'replace')
+            print('Iventory bulk table succesfully uploaded to HANA.')
+        except Exception as e:
+            print('Failed to upload Inventory bulk table to HANA: ' + str(e))
 
 def update_db_from_SAGE_command():
     update_db_from_SAGE_thread = threading.Thread(target = update_db_from_SAGE, daemon = True)
